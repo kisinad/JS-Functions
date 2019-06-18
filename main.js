725e528b5bd3c8d59c58ec1c315cccbe62238aad
Denis Kisina <kisinad@gmail.com>
2019-06-18 08:25:23 +0300

    added function

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //Number Guessing Game
 
-
+var i = Math.floor((Math.random() * 100) + 1); //Generates random numbers and stores it in i
 var randomNumber = function (){
-    var i = Math.floor((Math.random() * 100) + 1); //Generates random numbers and stores it in i
+    //var i = Math.floor((Math.random() * 100) + 1); //Generates random numbers and stores it in i
 
 	var j=i%2; //gets the modulus and stores the result in j
 	if (j==0) {//Conitioin checks to see if the result in module is zero or not
@@ -13,27 +13,27 @@ var randomNumber = function (){
 }
 
 var enterNumber = function(){
-    var i = Math.floor((Math.random() * 100) + 1); //Generates random numbers and stores it in i
-for(var count=1;count<=10;count++)
-{
-		let num = prompt ("Please enter a number:"); //Captures user input
-	if (num<i || num>100){
-		alert("Out of range");
-	}
+    //var i = Math.floor((Math.random() * 100) + 1); //Generates random numbers and stores it in i
+	for(var count=1;count<=10;count++)
+	{
+			let num = prompt ("Please enter a number:"); //Captures user input
+		if (num<1 || num>100){
+			alert("Out of range");
+		}
 
-	if (num===i){
-		alert("You WIN!");
-		break;
+		if (num===i){
+			alert("You WIN!");
+			break;
+		}
+		if (num<i){
+			alert("Number is lower, make another guess");
+		}
+		if (num>i){
+					alert("Number is higher,make another guess");
+				}
+		if (count==10){	
+				alert("You LOSE!");}
 	}
-	if (num<i){
-		alert("Number is lower, make another guess");
-	}
-	if (num>i){
-				alert("Number is higher,make another guess");
-			}
-	if (count==10){	
-			alert("You LOSE!");}
-}
 }
 randomNumber();
 enterNumber();
