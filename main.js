48ded884c10497f38a762e622e60e8e670bd473f
Denis Kisina <kisinad@gmail.com>
2019-06-18 09:41:42 +0300

    change the function type to arrow

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
 //Number Guessing Game
 
-var i = Math.floor((Math.random() * 100) + 1); //Generates random numbers and stores it in i
-var randomNumber = function (){
-    //var i = Math.floor((Math.random() * 100) + 1); //Generates random numbers and stores it in i
-
+//var i = Math.floor((Math.random() * 100) + 1); //Generates random numbers and stores it in i
+var randomNumber = () => {
+    var i = Math.floor((Math.random() * 100) + 1); //Generates random numbers and stores it in i
 	var j=i%2; //gets the modulus and stores the result in j
 	if (j==0) {//Conitioin checks to see if the result in module is zero or not
-		alert( i + " Number is Even"); }
+		alert ( i + " Number is Even"); }
 	else {
-		alert (i + " Number is odd");
+		alert  (i + " Number is odd");
 	}
-}
 
-var enterNumber = function(){
-    //var i = Math.floor((Math.random() * 100) + 1); //Generates random numbers and stores it in i
+}
+//var i = Math.floor((Math.random() * 100) + 1);
+var enterNumber = () => {
+    var i = Math.floor((Math.random() * 100) + 1); //Generates random numbers and stores it in i
 	for(var count=1;count<=10;count++)
 	{
-			let num = prompt ("Please enter a number:"); //Captures user input
+		let num = prompt ("Please enter a number:"); //Captures user input
 		if (num<1 || num>100){
-			alert("Out of range");
+			alert ("Out of range");
 		}
 
-		if (num===i){
-			alert("You WIN!");
+		if (num==i){
+			alert ("You WIN!");
 			break;
 		}
 		if (num<i){
-			alert("Number is lower, make another guess");
+			alert ("Number is lower, make another guess");
 		}
 		if (num>i){
-					alert("Number is higher,make another guess");
+			alert ("Number is higher,make another guess");
 				}
 		if (count==10){	
-				alert("You LOSE!");}
+			alert ("You LOSE!");}
 	}
 }
 randomNumber();
